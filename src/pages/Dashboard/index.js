9b846bd504ecd62b7b358e0a29ead1f2e677f143
raquelms203 <raquelmartins203@yahoo.com.br>
2020-02-15 18:35:41 -0200

    listagem politicos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,42 +1,89 @@
 import React, { useEffect, useState, useCallback } from "react";
 import {
-  Grid,
-  Paper,
   AppBar,
-  Toolbar,
-  IconButton,
   Button,
-  Typography,
-  MenuItem
+  List,
+  ListItem,
+  ListItemText
 } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
-import { apiStates } from "../../services/api";
+import { apiStates, apiADM } from "../../services/api";
 import { StyledGrid } from "./styles";
 
 export default function Dashboard() {
   const [cities, setCities] = useState([]);
+  const [indexPolitic, setIndexPolitic] = useState(1);
+  const [user, setUser] = useState({});
+  const [politics, setPolitics] = useState([]);
 
   const fetchCities = useCallback(async () => {
     if (cities.length === 0) {
-      const response = await apiStates.get();
-      const names = response.data.map(item => item.nome);
+      let response = await apiStates.get();
+      let names = response.data.map(item => item.nome);
       setCities(names);
     }
   }, [cities]);
 
+  const fetchUser = useCallback(async () => {
+    if (user.toSource() === "({})") {
+      let response = await apiADM.get();
+      let user = {
+        nome: response.data.nome,
+        partido: response.data.partido,
+        corPrimaria: response.data.corPrimaria
+      };
+      let politicsAll = [];
+
+      response.data.politicos.map(item => {
+        let p = {
+          nome: item.nome,
+          categoria: item.categoria,
+          cpf: item.cpf,
+          cidade: item.cidade
+        };
+        politicsAll.push(p);
+      });
+      setUser(user);
+      setPolitics(politicsAll);
+    }
+  }, [user, politics]);
+
   useEffect(() => {
     fetchCities();
-  }, [fetchCities]);
+    fetchUser();
+  }, [fetchCities, fetchUser]);
 
   return (
-    <AppBar position="static">
-      <div style={{ display: "flex" }}>
-        <Button style={{ marginLeft: "auto" }} color="inherit">
-          Login
-        </Button>
-      </div>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <div style={{ display: "flex" }}>
+          <Button style={{ marginLeft: "auto" }} color="inherit">
+            {user.nome}
+          </Button>
+        </div>
+      </AppBar>
+      <StyledGrid>
+        <List component="nav">
+          {politics.map(item => (
+            <ListItem button selected={indexPolitic === 0} onClick={() => {}}>
+              <ListItemText primary={item.nome}></ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </StyledGrid>
+    </>
   );
+}
+{
+  /* <StyledGrid>
+        <List component="nav" aria-label="main mailbox folders">
+          {politics.map((politic, index) => (
+            <ListItem button
+          selected={indexPolitic === index}
+          onClick={event => {}}>{politic}</ListItem>
+          ))}
+        </List>
+      </StyledGrid> */
 }
 // <StyledGrid container justify="center" alignItems="center">
 //   <Grid item xs={12} sm={6} md={3}>
--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const apiStates = axios.create({  
   baseURL: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/31/distritos"
+});
+
+export const apiADM = axios.create({  
+  baseURL: "http://localhost:3333/user"
 })
