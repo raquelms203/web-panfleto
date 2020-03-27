f8fff3da373e81837078a8db43009f043ab93e3f
raquelms203 <raquelmartins203@yahoo.com.br>
2020-03-27 18:50:11 -0200

    hired errors

---
+++ src/components/FormHired/validationSchema.js
@@ -0,0 +1,34 @@
+export const validateName = value => {
+  if (value === "") 
+  return "Campo obrigatório";
+  else if (!value.match(/^[A-Za-z" "]+$/)) {
+    return "Apenas letras permitidas";
+  } else 
+  return "";
+};
+
+export const validateEmail = value => {
+  if (value === "")
+   return "Campo obrigatório";
+  else if (!value.match(/^[^\s@]+@([^\s@.,]+\.)+[^\s@.,]{2,}$/)) 
+    return "Email inválido";
+   else 
+   return "";
+};
+
+export const validateMask = value => {  
+  if(value === "" || value.includes("_")) return "Campo obrigatório";
+
+  return "";
+}
+
+export const validateSelect = value => {  
+  if(value === "") return "Selecione uma opção";
+  else return "";
+}
+
+export const validateNotEmpty = value => {  
+  if(value === "") return "Campo obrigatório";
+  return "";
+}
+
