1eb9092229a9f8c8869896c1dcf614ab0e121d9f
raquelms203 <raquelmartins203@yahoo.com.br>
2020-08-25 21:36:15 -0300

    contrato

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1020,7 +1020,7 @@ export default function Dashboard() {
                   },
                   (index) => {
                     let url =
-                      `http://161.35.5.72/api/v1/contrato/${hireds[indexHired].id}/` +
+                      `http://econtraweb.com.br/contrato/${hireds[indexHired].id}/` +
                       `${managers[indexManager].id}/${politics[indexPolitic].id}`;
                     const win = window.open(url, "_blank");
                     if (win != null) {
